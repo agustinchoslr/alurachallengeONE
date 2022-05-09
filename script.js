3f5fdf5bb313fdd3cfae8f6d477c426afb70082f
agustinchoslr <agustinchoslr@gmail.com>
2022-05-09 20:30:21 -0300

    agregando función desencriptar

--- script.js
+++ script.js
@@ -17,3 +17,21 @@ for (let i = 0; i< matrizCodigo.length; i++){
 }
 return TextoAEncriptar;
 }
+
+//AHORA PARA DESENCRIPTAR==
+
+function btnDes(){
+    const textoEncriptado = desencriptar(inputTexto.value);
+    mensaje.textContent = textoEncriptado;
+}
+function desencriptar(TextoAEncriptar){
+   let matrizCodigo = [["enter", "e"], ["imes", "i"], ["ai", "a"], ["ober", "o"], ["ufat","u"]]
+   TextoAEncriptar = TextoAEncriptar.toLowerCase();
+for (let i = 0; i< matrizCodigo.length; i++){
+    if(TextoAEncriptar.includes(matrizCodigo[i][0])){
+        TextoAEncriptar = TextoAEncriptar.replaceAll
+        (matrizCodigo[i][0],matrizCodigo[i][1])
+    }
+}
+return TextoAEncriptar;
+}
